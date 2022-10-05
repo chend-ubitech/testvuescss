33d24cbc80b3bb1da766f4cef05a5b17e7d886cd
David <zdent09@gmail.com>
2022-10-05 08:07:35 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <div>hello</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.greetings {
+  color: $primary-color;
+}
+</style>
