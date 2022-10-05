61148692b16038b58a54a6c5c5c31f3bef309d73
david <david@ubitech.com.tw>
2022-10-05 10:02:35 +0800

    add index.scss

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/variables.scss";`
+        additionalData: `@import "@/assets/scss/index.scss";`
       }
     }
   },
